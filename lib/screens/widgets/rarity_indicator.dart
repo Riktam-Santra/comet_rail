df6949080f1696ed1fe9fdb18a07030da1374931
Riktam Santra <riktamsantra56@gmail.com>
2023-05-26 16:00:36 +0530

    changed rarityIndictor from being a simple function to a whole separate widget

--- lib/screens/widgets/rarity_indicator.dart
+++ lib/screens/widgets/rarity_indicator.dart
@@ -1,52 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-List<Widget> rarityIndicator(int rarity) {
-  switch (rarity) {
-    case 1:
-      return [
-        ImageIcon(
-          const AssetImage('assets/star.png'),
-          color: Colors.grey[700],
-        )
-      ];
+class RarityIndicator extends ConsumerWidget {
+  final int rarity;
+  const RarityIndicator({super.key, required this.rarity});
 
-    case 2:
-      return [
-        for (int i = 0; i < 2; i++)
-          ImageIcon(
-            const AssetImage('assets/star.png'),
-            color: Colors.teal[800],
-          )
-      ];
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    switch (rarity) {
+      case 1:
+        return Row(
+          children: [
+            ImageIcon(
+              const AssetImage('assets/star.png'),
+              color: Colors.grey[700],
+            )
+          ],
+        );
 
-    case 3:
-      return [
-        for (int i = 0; i < 3; i++)
-          ImageIcon(
-            const AssetImage('assets/star.png'),
-            color: Colors.blue[800],
-          )
-      ];
+      case 2:
+        return Row(
+          children: [
+            for (int i = 0; i < 2; i++)
+              ImageIcon(
+                const AssetImage('assets/star.png'),
+                color: Colors.teal[800],
+              )
+          ],
+        );
 
-    case 4:
-      return [
-        for (int i = 0; i < 4; i++)
-          ImageIcon(
-            const AssetImage('assets/star.png'),
-            color: Colors.purple[800],
-          )
-      ];
+      case 3:
+        return Row(
+          children: [
+            for (int i = 0; i < 3; i++)
+              ImageIcon(
+                const AssetImage('assets/star.png'),
+                color: Colors.blue[800],
+              )
+          ],
+        );
 
-    case 5:
-      return [
-        for (int i = 0; i < 5; i++)
-          ImageIcon(
-            const AssetImage('assets/star.png'),
-            color: Colors.yellow[800],
-          )
-      ];
+      case 4:
+        return Row(
+          children: [
+            for (int i = 0; i < 4; i++)
+              ImageIcon(
+                const AssetImage('assets/star.png'),
+                color: Colors.purple[800],
+              )
+          ],
+        );
 
-    default:
-      return [];
+      case 5:
+        return Row(
+          children: [
+            for (int i = 0; i < 5; i++)
+              ImageIcon(
+                const AssetImage('assets/star.png'),
+                color: Colors.yellow[800],
+              )
+          ],
+        );
+
+      default:
+        return Container();
+    }
   }
 }
