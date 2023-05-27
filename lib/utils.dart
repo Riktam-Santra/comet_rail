6e33c81d5cb6cd072ecc9e3ac3c2cc825c60167a
Riktam Santra <riktamsantra56@gmail.com>
2023-05-28 00:20:41 +0530

    modified parseStatInDesc to properly allot values including float values.

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 abstract class Utils {
   static String convertPathName(String path) {
     switch (path) {
@@ -22,29 +24,72 @@ abstract class Utils {
 
   static String parseStatInLightConeDesc(
       String data, List<List<double>> params) {
-    data.replaceAll(RegExp(r'\\n'), "\n");
-    if (data.contains(RegExp(r'#[0-9]+\[i\]'))) {
-      List<String> dataVec = data.split(" ");
-      int index = 0;
-      for (final word in dataVec) {
-        if (word.contains(RegExp(r'#[0-9]+\[i\]'))) {
-          if (word.contains("%")) {
-            final wordVec = word.split("[i]");
-            final skillIndex = int.parse(wordVec[0].replaceAll("#", ""));
-            dataVec[index] =
-                "${(params[0][skillIndex - 1] * 100).toInt().toString()}%";
-          } else {
-            final wordVec = word.split("[i]");
-            final skillIndex = int.parse(wordVec[0].replaceAll("#", ""));
-            dataVec[index] = (params[0][skillIndex - 1]).toInt().toString();
-          }
-        }
-        index++;
-      }
+    RegExp normalExp = RegExp(r'#[0-9]+\[i\]');
 
-      return dataVec.join(" ");
+    RegExp percentExp = RegExp(r'#[0-9]+\[i\]%');
+
+    RegExp floatExp = RegExp(r'#[0-9]+\[f\]');
+
+    List<Match> normalMatches = normalExp.allMatches(data).toList();
+    Iterable<Match> percentMatches = percentExp.allMatches(data);
+    List<Match> floatMatches = floatExp.allMatches(data).toList();
+
+    normalMatches.removeWhere((element) {
+      for (final match in percentMatches) {
+        if (match[0]!.contains(element[0]!)) {
+          return true;
+        }
+      }
+      return false;
+    });
+
+    String iterString = data;
+
+    for (final match in normalMatches) {
+      log(match[0] ?? 'no match value');
+      String numString = match[0]!
+          .replaceAll("#", "")
+          .replaceAll("[i]", "")
+          .replaceAll("[f]", "");
+
+      int index = int.parse(numString) - 1;
+
+      log(numString);
+      iterString =
+          iterString.replaceAll(match[0]!, params[0][index].toString());
     }
-    return data;
+
+    for (final match in percentMatches) {
+      log(match[0] ?? 'no match value');
+      String numString = match[0]!
+          .replaceAll("#", "")
+          .replaceAll("[i]", "")
+          .replaceAll("[f]", "")
+          .replaceAll("%", "");
+
+      int index = int.parse(numString) - 1;
+
+      log(numString);
+      iterString = iterString.replaceAll(
+          match[0]!, "${(params[0][index] * 100).toString()}%");
+    }
+
+    for (final match in floatMatches) {
+      log(match[0] ?? 'no match value');
+      String numString = match[0]!
+          .replaceAll("#", "")
+          .replaceAll("[i]", "")
+          .replaceAll("[f]", "")
+          .replaceAll("%", "");
+
+      int index = int.parse(numString) - 1;
+
+      log(numString);
+      iterString =
+          iterString.replaceAll(match[0]!, (params[0][index]).toString());
+    }
+
+    return iterString;
   }
 
   static String parseStatInDesc(String data, List<double> params) {
